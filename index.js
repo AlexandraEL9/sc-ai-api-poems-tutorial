8a6498e412526ab73c80f8003e0584638e099f4e
Alexandra Lee <alexandraelee9@gmail.com>
2024-07-03 11:32:54 +0000

    edit prompt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function showAnswer(response) {
   let apiKey = "06a59897dof9572ebabb740ftb4354ba";
   let context =
     "be polite and provide a very short answer. make sure to pick one";
-  let prompt = "what's the best cuisine in the world?";
+  let prompt = "what's the best healthiest cuisine in the world?";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Processing")
   axios.get(apiUrl).then(showAnswer);
