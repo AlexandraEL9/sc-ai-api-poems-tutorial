5ddf744ba8841cb303d72a92c605ec57625ccea5
Alexandra Lee <alexandraelee9@gmail.com>
2024-07-05 15:58:49 +0000

    add ss with js

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function showAnswer(response) {
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Processing")
   axios.get(apiUrl).then(showAnswer);
+
+  //css in javaScript
+  function handleClick() {
+    alert("hooray");
+  }
+  let buttonElement = document.querySelector("#special-button");
+  buttonElement.addEventListener("click", handleClick);
+  buttonElement.style.background = "red";
+
+  //challenge
+  //chal 1
+var challeng1Button = document.querySelector("#challenge-1");
+challeng1Button.setAttribute("disabled", "disabled");
+
+//chall 2
+var challenge2Button = document.querySelector("#challenge-2");
+challenge2Button.style.backgroundColor = "green";
+
+// chall 3
+var challenge3Button = document.querySelector("#challenge-3");
+challenge3Button.classList.add("danger");
