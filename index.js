4fc9ac0b2bfe249f856bdb9451d04bd4456fdfe9
Alexandra Lee <alexandraelee9@gmail.com>
2024-07-07 20:25:26 +0000

    joke generator homework

--- index.js
+++ index.js
@@ -41,3 +41,34 @@ new Typewriter('h1', {
   cursor: "",
   delay: 10,
 });
+
+//Homework task week 1- Joke Generator
+function displayJoke(response) {
+  console.log(response.data.answer);
+
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
+function generateJoke(event) {
+  event.preventDefault();
+  let apiKey = "06a59897dof9572ebabb740ftb4354ba";
+  let context =
+    "Your are a funny AI Assistant that tells short and sweet jokes. The joke must be provided in HTML format. Example: <p>this is a joke</p>";
+  let prompt = "Generate a short and unique joke a French people. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeElement = document.querySelector("#joke");
+
+  jokeElement.innerHTML = "Generating a joke for you.. please wait";
+
+  console.log("called the AI api");
+  axios.get(apiUrl).then(displayJoke);
+}
+
+let generatorButton = document.querySelector("#make-joke-button");
+generatorButton.addEventListener("click", generateJoke);
