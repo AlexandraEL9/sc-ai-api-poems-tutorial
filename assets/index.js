4c00a262105f51fb14bdfeb7a8dce55317e274ff
Alexandra Lee <alexandraelee9@gmail.com>
2024-07-09 16:55:32 +0000

    add ai api

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,31 @@
-alert("Hello");
+function displayPoem(response) {
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+
+}
 
 function generatePoem(event) {
     event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+
+    //build api url
+    let apiKey = "06a59897dof9572ebabb740ftb4354ba";
+    let context = "You are a romantic poetry expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions.";
+    let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    let apiUrl = 
+        `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    alert(`Generating Poem about ${prompt} Please wait...`)
+
+    //make a call to the api (axios)
+    axios.get(apiUrl).then(displayPoem);
+    //display generated poem
   
-    new Typewriter("#poem", {
-      strings: "La tombe dit à la rose",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
   }
   
   let poemFormElement = document.querySelector("#poem-generator-form");
